cc12be5da217b29ada8170b8acd6815e5cf94095
Samadi van Koten <samadi@vktec.org.uk>
2021-07-26 17:47:04 +0100

    Force build mode to ReleaseSafe to workaround dumb codegen bug

--- build.zig
+++ build.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 const classgen = @import("classgen.zig");
 
 pub fn build(b: *std.build.Builder) void {
-    const mode = b.standardReleaseOptions();
+    const mode = std.builtin.Mode.ReleaseSafe; // ziglang/zig#7935
     const target = b.standardTargetOptions(.{
         .default_target = std.zig.CrossTarget.parse(.{
             .arch_os_abi = "i386-native",
@@ -10,7 +10,6 @@ pub fn build(b: *std.build.Builder) void {
     });
 
     const lib = b.addSharedLibrary("wormhole", "src/main.zig", .unversioned);
-    lib.force_pic = true;
     lib.addPackage(classgen.pkg(b, "sdk", "sdk"));
     lib.setBuildMode(mode);
     lib.setTarget(target);
